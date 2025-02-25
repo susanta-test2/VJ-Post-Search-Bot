5cefcbd63571a91f03bb3872fb6064c1df46bcb2
susanta-test2 <sarkarbalaram409@gmail.com>
2025-02-25 14:21:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+API_ID       = int(environ.get("API_ID", "21134445"))
+API_HASH     = environ.get("API_HASH", "231c18ea7273824491d6bf05425ab74e")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "8194500582:AAENIU65T-Fs6pv4Ez_NqOqAr6PhQIZfY1U")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://bornhyper1:Bornhyper5911@cluster0.gmvpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002421090499"))
+ADMIN        = int(environ.get("ADMIN", "7763229951"))
+CHANNEL      = environ.get("CHANNEL", "@animechillax")
